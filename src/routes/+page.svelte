d784ee0ec382e009f722890a0b31284502600e99
ryanlinjui <ryanlinjui@gmail.com>
2023-06-13 19:54:22 +0800

    feat: try-btn detect login status

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,15 @@
 	import { t } from "svelte-i18n";
 	import { page } from "$app/stores";
 	import Head from "$lib/component/Head.svelte";
+	import type { Route } from "@playwright/test";
+	import type { PageData } from "./$types";
 
 	const callback = new URL("/auth", $page.url).toString();
+	export let data: PageData;
+	const try_btn_link =
+		data.user == null
+			? `https://pea.csie.cool/app/privy-council-llm?cb=${callback}`
+			: `/@${data.user.namespace_name}/manage`;
 </script>
 
 <Head />
@@ -15,7 +22,7 @@
 
 		<hr />
 
-		<a class="btn-primary btn" href="https://pea.csie.cool/app/privy-council-llm?cb={callback}">
+		<a class="btn-primary btn" href={try_btn_link}>
 			{$t("intro.try-it-out")}
 		</a>
 	</div>
