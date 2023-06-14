8bec1d5df00cb44b57f557b9dfd73bb11031eb0f
ryanlinjui <ryanlinjui@gmail.com>
2023-06-15 07:53:21 +0800

    feat: optimize UI

--- src/routes/create-team/+page.svelte
+++ src/routes/create-team/+page.svelte
@@ -2,16 +2,20 @@
 	import { t } from "svelte-i18n";
 </script>
 
-<div class="flex h-full w-full items-center justify-center">
-	<div class="prose">
-		<h1>{$t("create-new-team.title")}</h1>
-		<form method="POST">
-			<h2 style="padding-top:10%">
-				{$t("name")}
-				<input name="name" type="text" class="input w-full max-w-xs" />
-			</h2>
-			<button class="btn">{$t("create")}</button>
-		</form>
-		<p>{$t("create-new-team.description")}</p>
+<div class="flex h-full w-full items-center justify-center p-4">
+	<div class="w-full max-w-2xl">
+		<div class="prose mb-8">
+			<h1>{$t("create-new-team.title")}</h1>
+		</div>
+		<div class="flex w-full gap-4 max-sm:flex-col-reverse">
+			<form method="POST" class="flex flex-1 flex-col gap-4">
+				<label>
+					{$t("name")}
+					<input name="name" type="text" class="input w-full" />
+				</label>
+				<button class="btn-primary btn">{$t("create")}</button>
+			</form>
+			<p class="w-40">{$t("create-new-team.description")}</p>
+		</div>
 	</div>
 </div>
