64158d3a439f1686478f014835b5eabdc2b54544
ryanlinjui <ryanlinjui@gmail.com>
2023-06-15 12:10:26 +0800

    src/routes/@[namespace]/committee
    feat: optimize UI

--- src/routes/@[namespace]/library/new/+page.svelte
+++ src/routes/@[namespace]/library/new/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import { t } from "svelte-i18n";
+	import Markdown from "svelte-markdown";
 </script>
 
 <div class="flex h-full w-full items-center justify-center p-4">
-	<div class="w-full max-w-2xl">
+	<div class="w-full max-w-4xl">
 		<div class="prose mb-8">
 			<h1>{$t("create-new-library.title")}</h1>
 		</div>
@@ -15,7 +16,9 @@
 				</label>
 				<button class="btn-primary btn">{$t("create")}</button>
 			</form>
-			<p class="w-40">{$t("create-new-library.description")}</p>
+			<div class="prose max-w-sm">
+				<Markdown source={$t("create-new-library.description")} />
+			</div>
 		</div>
 	</div>
 </div>
